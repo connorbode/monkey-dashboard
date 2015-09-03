562ee82e5831690e25e907281d575ff21a740602
Connor Bode <connor.bode@gmail.com>
2015-09-03 09:44:58 -0400

    babel for tests

--- spec/core.spec.js
+++ spec/core.spec.js
@@ -1,3 +1,11 @@
-before(function () {
+var expect = require('chai').expect;
 
+before(() => {
+
+});
+
+describe('tests', () => {
+  it('should pass', () => {
+    expect(true).to.equal(true);
+  });
 });
