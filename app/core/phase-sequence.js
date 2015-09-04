f755fc1a4ba0d1ea77892f1fd90f25a2ac7f1967
Connor Bode <connor.bode@gmail.com>
2015-09-04 11:19:43 -0400

    add phase sequence methods

--- app/core/phase-sequence.js
+++ app/core/phase-sequence.js
@@ -1,3 +1,5 @@
+var EventEmitter = require('events').EventEmitter;
+
 //
 // PhaseSequence
 // -------------
@@ -5,7 +7,61 @@
 // A PhaseSequence is based on a PhaseSet, but
 // includes repititions of tasks within the set.
 //
-
 var PhaseSequence = function (set) {
   this.set = set;
+  this.phases = [];
+
+  this.EVENTS = {
+    ADDED: 'added',
+    REMOVED: 'removed'
+  };
+
+  //
+  // removeAllById
+  // -------------
+  //
+  // Removes all instances of a phase
+  // from the sequence.
+  //
+  var removeAllById = (phase) => {
+    this.phases = this.phases.reduce((phases, p) => {
+      if (p.id !== phase.id)
+        phases.push(p);
+      return phases;
+    }, []);
+  };
+
+  //
+  // add
+  // ---
+  //
+  // Adds a phase to the sequence
+  //
+  this.add = (phase) => {
+    if (this.set.indexOf(phase) === -1)
+      throw "the phase provided was not part of the set..";
+
+    this.phases.push(phase);
+    this.emit(this.EVENTS.ADDED, phase);
+  };
+
+  //
+  // removeByIndex
+  // -------------
+  //
+  // Removes a phase from the sequence using the index.
+  //
+  this.removeByIndex = (index) => {
+    if (index > this.phases.length)
+      throw "index out of bounds";
+
+    var spliced = this.phases.splice(index, 1);
+    this.emit(this.EVENTS.REMOVED, spliced[0]);
+  };
+
+  this.set.on(this.set.EVENTS.REMOVED, removeAllById);
 };
+
+PhaseSequence.prototype = Object.create(EventEmitter.prototype);
+
+module.exports = PhaseSequence;
---
+++ app/core/phase-sequence.spec.js
@@ -0,0 +1,86 @@
+var chai = require('chai');
+var expect = chai.expect;
+var PhaseSequence = require('./phase-sequence');
+var PhaseSet = require('./phase-set');
+var Phase = require('./phase');
+
+describe('PhaseSequence', () => {
+
+  var phase1,
+      phase2,
+      set;
+
+  beforeEach(() => {
+    phase1 = new Phase({
+      duration: 1000,
+      name: 'phase1'
+    });
+
+    phase2 = new Phase({
+      duration: 1000,
+      name: 'phase2'
+    });
+
+    set = new PhaseSet();
+  });
+
+  it('throws an error if the phase provided is not part of the phase set', () => {
+    set.add(phase1);
+    var sequence = new PhaseSequence(set);
+    expect(sequence.add.bind(null, phase2)).to.throw;
+  });
+
+  it('adds a phase to the sequence', () => {
+    set.add(phase1);
+    var sequence = new PhaseSequence(set);
+    sequence.add(phase1);
+    expect(sequence.phases.length).to.equal(1);
+    expect(sequence.phases[0].name).to.equal('phase1');
+  });
+
+  it('emits a changed event when a phase gets added to the sequence', done => {
+    set.add(phase1);
+    var sequence = new PhaseSequence(set);
+    sequence.on(sequence.EVENTS.ADDED, (p) => {
+      expect(p.id).to.equal(phase1.id);
+      done();
+    });
+    sequence.add(phase1);
+  });
+
+  it('throws an error if trying to remove an index that is out of bounds', () => {
+    set.add(phase1);
+    var sequence = new PhaseSequence(set);
+    expect(sequence.removeByIndex.bind(null, 3)).to.throw;
+  });
+
+  it('removes a phase from the sequence by its index', () => {
+    set.add(phase1);
+    set.add(phase2);
+    var sequence = new PhaseSequence(set);
+    sequence.phases.push(phase1, phase2, phase1);
+    sequence.removeByIndex(1);
+    expect(sequence.phases.length).to.equal(2);
+    expect(sequence.phases[0].name).to.equal('phase1');
+    expect(sequence.phases[1].name).to.equal('phase1');
+  });
+
+  it('emits a changed event when a phase is removed from the sequence', done => {
+    set.add(phase1);
+    var sequence = new PhaseSequence(set);
+    sequence.phases.push(phase1);
+    sequence.on(sequence.EVENTS.REMOVED, (p) => {
+      expect(p.id).to.equal(phase1.id);
+      done();
+    });
+    sequence.removeByIndex(0);
+  });
+
+  it('removes any instances of a phase from the sequence when the sequence is removed from the set', () => {
+    set.add(phase1);
+    var sequence = new PhaseSequence(set);
+    sequence.phases.push(phase1, phase1, phase1, phase1);
+    set.emit(set.EVENTS.REMOVED, phase1);
+    expect(sequence.phases).to.be.empty;
+  });
+});
