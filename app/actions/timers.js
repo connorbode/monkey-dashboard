8ffd36154936643abee48b4b2b3cf3779a42ca97
Connor Bode <connor.bode@gmail.com>
2015-09-12 11:13:26 -0400

    add timer actions

---
+++ app/actions/timers.js
@@ -0,0 +1,14 @@
+var timerActions = {
+
+  // loads all the possible timers into the application
+  load: () => {
+
+  },
+
+  // saves all of the timers
+  save: (timers) => {
+    
+  }
+};
+
+module.exports = timerActions;
