df671e8d918cc54da746be8125fd901b2ae08f27
Connor Bode <connor.bode@gmail.com>
2015-09-13 21:19:49 -0400

    adding timers

--- app/actions/timers.js
+++ app/actions/timers.js
@@ -1,13 +1,16 @@
+var Cookies = require('cookies-js');
+var TIMERS_COOKIE = 'workhorse_timers';
+
 var timerActions = {
 
   // loads all the possible timers into the application
   load: () => {
-
+    return Cookies.get(TIMERS_COOKIE);
   },
 
   // saves all of the timers
   save: (timers) => {
-    
+    Cookies.set(TIMERS_COOKIE, timers);
   }
 };
 
