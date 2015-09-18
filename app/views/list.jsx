4185c8b8ba2da9426eb311acbafbb134dad06c67
Connor Bode <connor.bode@gmail.com>
2015-09-18 12:59:07 -0400

    fixed up compilation

--- app/views/list.jsx
+++ app/views/list.jsx
@@ -2,26 +2,25 @@ var React      = require('react/addons');
 var TimerStore = require('../stores/timer-store');
 
 var list = React.createClass({
-  loadTimers: () => {
+  loadTimers: function () {
     var state = this.state;
     state.timers = TimerStore.timers;
     this.setState(state);
   },
 
-  getInitialState: () => {
+  getInitialState: function () {
     return {
       timers: []
     };
   },
 
-  componentDidMount: () => {
+  componentDidMount: function () {
     TimerStore.on(TimerStore.events.CHANGED, this.loadTimers);
   },
 
-  render: () => {
+  render: function () {
     return (
       <div className="timer-list">
-        TIMERS
         <ul>
           {this.state.timers.map(timer => {
             return (
