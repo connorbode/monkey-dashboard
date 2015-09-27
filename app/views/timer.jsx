15b6e7c679542dda1be6ee961a2aa0be276865a5
Connor Bode <connor.bode@gmail.com>
2015-09-27 12:01:01 -0400

    full circle

--- app/views/timer.jsx
+++ app/views/timer.jsx
@@ -5,7 +5,7 @@ var Timer = React.createClass({
     return (
       <svg className="timer">
         <path
-          d="M 100 50 A 50 50, 0, 1, 0, 50 100" 
+          d="M 100 50 A 50 50, 0, 1, 0, 100 49 Z" 
           fill="transparent"
           stroke="black"
           stroke-width="1" />
