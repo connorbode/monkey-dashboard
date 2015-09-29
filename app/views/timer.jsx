072726aad46398565a0d0258f0b4b564cb4fea98
Connor Bode <connor.bode@gmail.com>
2015-09-28 21:19:10 -0400

    working on circle timer

--- app/views/timer.jsx
+++ app/views/timer.jsx
@@ -1,11 +1,17 @@
 var React = require('react/addons');
 
 var Timer = React.createClass({
+  getInitialState: function () {
+    return {
+      fill: 0.25
+    };
+  },
+
   render: function () {
     return (
       <svg className="timer">
         <path
-          d="M 100 50 A 50 50, 0, 1, 0, 100 49 Z" 
+          d="M 100 50 A 50 50, 0, 1, 0, 100 49 Z"
           fill="transparent"
           stroke="black"
           stroke-width="1" />
